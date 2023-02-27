8e2fe4786864f19743d2e04a178fa2863d12b777
3345-c-a-t-s-u-s <117000269+3345-c-a-t-s-u-s@users.noreply.github.com>
2023-02-27 00:38:15 -0800

    Update Book_1_Full.lua

--- Book_1_Full.lua
+++ Book_1_Full.lua
@@ -1,11 +1,15 @@
-print('Script Loading...')
-if game.PlaceId == 6301638949 or game.PlaceId == 6296321810 then
+print(' Script Loading...')
+if game.PlaceId == 6296321810 or game.PlaceId == 6301638949 then
    -- Chapter 1
    loadstring(game:HttpGet"https://raw.githubusercontent.com/3345-c-a-t-s-u-s/mimic/main/Ch1_scr.lua")()
-   elseif game.PlaceId == 6373539583 or game.PlaceId == 6406571212 or game.PlaceId == 6425178683 then
+end
+   if game.PlaceId == 6373539583 or game.PlaceId == 6406571212 or game.PlaceId ==     6425178683 then
    -- Chapter 2
    loadstring(game:HttpGet"https://raw.githubusercontent.com/3345-c-a-t-s-u-s/mimic/main/Ch2_.lua")()
-     elseif game.PlaceId == 6425178683 or game.PlaceId == 6682163754 or game.PlaceId == 6682164423 then
+   end
+   
+if game.PlaceId == 6472459099 or game.PlaceId == 6682163754 or game.PlaceId == 6682164423 then
      -- Chapter 3
       loadstring(game:HttpGet"https://raw.githubusercontent.com/3345-c-a-t-s-u-s/mimic/main/Ch3_scr.lua")()
 end
+
